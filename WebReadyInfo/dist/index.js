969dc8f7785db9692657c7c3cfaec127a29254f0
thebishes <59317215+thebishes@users.noreply.github.com>
2025-05-19 07:06:52 +0100

    Add files via upload

--- WebReadyInfo/dist/index.js
+++ WebReadyInfo/dist/index.js
@@ -92,7 +92,7 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true
   }
 });

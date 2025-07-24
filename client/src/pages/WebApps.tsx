342cba7d45866d905352dc188caad343c49bbe9b
thebishes <59317215+thebishes@users.noreply.github.com>
2025-07-24 21:01:53 +0100

    Updated WebApps.tsx

--- client/src/pages/WebApps.tsx
+++ client/src/pages/WebApps.tsx
@@ -15,7 +15,7 @@ const WebApps = () => {
 
   const webAppShowcase = [
     {
-      image: "./media1.png",
+      image: "/dist/assets/media1.png",
       title: "User control built in",
       description: "Empowering you to control who has access to your web application",
       features:["Team Control", "User Control"]
